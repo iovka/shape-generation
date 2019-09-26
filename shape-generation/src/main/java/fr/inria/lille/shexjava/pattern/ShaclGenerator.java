a03cde6c4d5097905280f00eacddf334e4323253
Jérémie Dusart <jeremie.dusart@inria.fr>
2019-09-26 15:14:11 +0200

    Updating pom for central repository release.

--- shape-generation/src/main/java/fr/inria/lille/shexjava/pattern/ShaclGenerator.java
+++ shape-generation/src/main/java/fr/inria/lille/shexjava/pattern/ShaclGenerator.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 import org.apache.commons.rdf.api.Graph;
 import org.apache.commons.rdf.api.RDFTerm;
+import org.eclipse.rdf4j.model.Model;
 
 import fr.inria.lille.shexjava.pattern.abstrt.Pattern;
 import fr.inria.lille.shexjava.pattern.indications.PatternInstantiation;
-import fr.inria.lille.shexjava.pattern.indications.ShexFromPatternConstructor;
-import fr.inria.lille.shexjava.schema.abstrsynt.ShapeExpr;
+import fr.inria.lille.shexjava.pattern.indications.SHACLFromPatternConstructor;
 
 
-public class ShexInstantiator {
+public class ShaclGenerator {
 	
-	public static ShapeExpr instantiate (Graph graph, List<RDFTerm> sample, Pattern pattern ) {
+	public static Model instantiate (Graph graph, List<RDFTerm> sample, Pattern pattern, String baseIRI ) {
 		PatternInstantiation pi = new PatternInstantiation(pattern, sample, graph);
-		ShexFromPatternConstructor shexConstructor = new ShexFromPatternConstructor();
+		SHACLFromPatternConstructor shexConstructor = new SHACLFromPatternConstructor(baseIRI);
 		return shexConstructor.construct(pi);
 	}
 	
